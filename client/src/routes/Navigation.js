e2b0ca89d1e82253f390fe03e6cb7c0ee038a0ac
ferdeolazabal <ferdeolazabal@gmail.com>
2024-03-31 19:30:59 -0300

    Delete comments

--- client/src/routes/Navigation.js
+++ client/src/routes/Navigation.js
@@ -131,36 +131,6 @@ export const Navigation = () => {
             </NavLink>
           </nav>
         </div>
-        {/* <nav>
-          <img src={ logo } className="App-logo" alt="logo" />
-
-          <ul>
-            <li>
-              <NavLink
-                to="/home"
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/users"
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Users
-              </NavLink>
-            </li>
-          </ul>
-        </nav> */}
 
         <Routes>
           <Route path="payments" element={<h1>Payments Page</h1>} />

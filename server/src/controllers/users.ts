dc68530f0744daa36d9b6ce0b5de04ead7fec9b6
ferdeolazabal <ferdeolazabal@gmail.com>
2024-04-05 01:51:39 -0300

    Add functionality to get user by id
    - Add route

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,5 +1,8 @@
 import { AppDataSource } from "../data-source";
+import { generateJWT } from "../helpers/jwt";
 import { Response, Request } from "express";
+import bcrypt from "bcryptjs";
+
 import User from "../domain/User";
 import UserORMEntity from "../Infrastructure/Entities/UserORMEntity";
 
@@ -21,4 +24,84 @@ const getUsers = async (req: Request, res: Response) => {
   }
 };
 
-export { getUsers };
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const savedUser = await AppDataSource.manager.findOne(UserORMEntity, {
+      where: { _id: id },
+    });
+    const user = User.fromValues(savedUser);
+
+    res.json({
+      ok: true,
+      user,
+    });
+  } catch (e) {
+    console.log({ e });
+    res.status(500).json({
+      ok: false,
+      message: "Error al obtener usuario",
+    });
+  }
+};
+
+// const postUser = async (req: Request, res: Response) => {
+//   const { body } = req;
+
+//   try {
+//     const existeEmail = await AppDataSource.manager.findOne(UserORMEntity, {
+//       where: {
+//         email: body.email,
+//       },
+//     });
+
+//     if (existeEmail) {
+//       return res.status(400).json({
+//         ok: false,
+//         message: `El email ${body.email} ya está registrado`,
+//       });
+//     }
+
+//     const user = new User();
+
+//     const salt = bcrypt.genSaltSync();
+
+//     user.firstName = body.firstName;
+//     user.lastName = body.lastName;
+//     user.email = body.email;
+//     user.password = bcrypt.hashSync(body.password, salt);
+
+//     user.enable = true;
+//     user.isSuperAdmin = false;
+
+//     // const newUser = await AppDataSource.manager.save(
+//     const saveUser = await AppDataSource.manager.save(
+//       new UserORMEntity(),
+//       // new User(),
+//       { data: user }
+//     );
+//     console.log("saveUser", { saveUser });
+//     const newUser = User.fromValues(saveUser);
+
+//     console.log("newUser", { newUser });
+
+//     const token = await generateJWT(newUser.getId(), newUser.getFullName());
+
+//     console.log("token", token);
+
+//     res.json({
+//       ok: true,
+//       newUser,
+//       token,
+//     });
+//   } catch (e) {
+//     console.log({ e });
+//     res.status(500).json({
+//       ok: false,
+//       message: "Error al guardar el usuario",
+//     });
+//   }
+// };
+
+export { getUsers, getUser };
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users";
+// import { check } from "express-validator";
+// import { fieldValidator } from "../middlewares/fieldValidator";
+import { getUsers, getUser } from "../controllers/users";
 
 const usersRoutes = Router();
 
 usersRoutes.get("/", getUsers);
+
+usersRoutes.get("/:id", getUser);
+
+// usersRoutes.post(
+//   "/new",
+//   [
+//     check("firstName", "El nombre es obligatorio").not().isEmpty(),
+//     check("lastName", "El apellido es obligatorio").not().isEmpty(),
+//     check("email", "El email es obligatorio").isEmail(),
+//     check("password", "El password debe de ser de 6 caracteres").isLength({
+//       min: 6,
+//     }),
+//     fieldValidator,
+//   ],
+//   postUser
+// );
 
 export default usersRoutes;

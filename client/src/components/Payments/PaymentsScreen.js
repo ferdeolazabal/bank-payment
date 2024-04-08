b5d1d59db102ad7c4b28a15a0ff06be57eb51bc9
ferdeolazabal <ferdeolazabal@gmail.com>
2024-04-07 23:22:47 -0300

    Add new payment form

--- client/src/components/Payments/PaymentsScreen.js
+++ client/src/components/Payments/PaymentsScreen.js
@@ -3,12 +3,16 @@ import React from "react";
 import PaymentsRow from "./PaymentsRow";
 import PaymentsHeader from "./PaymentsHeader";
 import { useSelector } from "react-redux";
+import PaymentModal from "../UiElements/PaymentModal";
 
 const PaymentsScreen = () => {
   const payments = useSelector(({ payments }) => payments.payments);
 
   return (
     <div className="container px-6 py-8 mx-auto">
+      <div className="flex justify-end">
+        <PaymentModal />
+      </div>
       <div className="flex flex-col mt-8">
         <div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
           <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
---
+++ client/src/components/UiElements/PaymentModal.js
@@ -0,0 +1,120 @@
+// @ts-nocheck
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { Field, Form, Formik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { httpPostPayment } from "../../redux/actions/payments";
+
+const PaymentModal = () => {
+  const dispatch = useDispatch();
+  const [initialValues, setInitialValues] = useState({
+    amount: "",
+    type: "",
+    receiver: "",
+  });
+
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  let formikRef = null;
+
+  const users = useSelector(({ users }) => users.users) || [];
+
+  const handleSubmit = (values) => {
+    Object.assign(values, {
+      amount: values.amount.toString().replace(/\./g, ""),
+      user: users[0],
+      status: "success",
+    });
+    dispatch(httpPostPayment(values));
+  };
+
+  const showSwal = () => {
+    withReactContent(Swal).fire({
+      title: "Registrar Pago",
+      showCancelButton: true,
+      html: (
+        <Formik
+          innerRef={(ref) => (formikRef = ref)}
+          initialValues={initialValues}
+          validate={({ amount, type, receiver }) => {
+            const errors = {};
+            if (!amount) {
+              errors.monto = "Monto requerido";
+            }
+            if (!type) {
+              errors.tipo = "Tipo requerido";
+            }
+            if (!emailRegex.test(receiver)) {
+              errors.receptor = "El destinatario debe ser un email";
+            }
+            return errors;
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Field
+              type="number"
+              className="swal2-input"
+              placeholder="Monto"
+              name="amount"
+              onKeyPress={(event) =>
+                event.key === "Enter" && Swal.clickConfirm()
+              }
+            />
+
+            <Field
+              as="select"
+              className="swal2-select"
+              name="type"
+              onKeyPress={(event) =>
+                event.key === "Enter" && Swal.clickConfirm()
+              }
+            >
+              <option value="">Tipo</option>
+              <option value="creditCard">Tarjeta de crédito</option>
+              <option value="wallet">Billetera vitual</option>
+              <option value="bankTransfer">Transferencia bancaria</option>
+            </Field>
+            <Field
+              type="email"
+              className="swal2-input"
+              placeholder="Email destinatario"
+              name="receiver"
+              onKeyPress={(event) =>
+                event.key === "Enter" && Swal.clickConfirm()
+              }
+            />
+          </Form>
+        </Formik>
+      ),
+      didOpen: () => {
+        Swal.getPopup()?.querySelector("input")?.focus();
+        setInitialValues(initialValues);
+      },
+      preConfirm: async () => {
+        await formikRef?.submitForm();
+        if (formikRef?.isValid) {
+          Swal.fire({
+            title: "Pago Enviado!",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+          });
+        } else {
+          Swal.showValidationMessage(JSON.stringify(formikRef?.errors));
+        }
+      },
+    });
+  };
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={showSwal}
+    >
+      Registrar Pago
+    </button>
+  );
+};
+
+export default PaymentModal;

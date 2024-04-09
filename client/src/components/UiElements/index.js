8739370b739c879fb704146a278c60f3cc54890e
ferdeolazabal <ferdeolazabal@gmail.com>
2024-04-08 21:10:21 -0300

    Delete unused icon

--- client/src/components/UiElements/index.js
+++ client/src/components/UiElements/index.js
@@ -1,6 +1,5 @@
 import Logo from "./Logo";
-import PieIcon from "./PieIcon";
 import UserIcon from "./UserIcon";
 import PayIcon from "./PayIcon";
 
-export { Logo, PieIcon, UserIcon, PayIcon };
+export { Logo, UserIcon, PayIcon };
